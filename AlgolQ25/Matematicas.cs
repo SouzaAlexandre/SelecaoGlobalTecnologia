6b75c1d0f413cfd60c8dbaa47b40132bb7c923df
Alexandre Souza <43093590+SouzaAlexandre@users.noreply.github.com>
2021-01-27 18:15:25 -0300

    Adicionar arquivos de projeto.

---
+++ AlgolQ25/Matematicas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgolQ25
+{
+    public static class Matematicas
+    {
+       public static int RetornaMenorDivisivel(int DivisorMaximo)
+        {
+            int valor = 0;
+            bool ok = false;
+
+            while (!ok)
+            {
+                valor++; 
+                for (int i = 1; i <= DivisorMaximo; i++)
+                {
+                    if(valor % i != 0)
+                    {
+                        ok = false;
+                        break;
+                    } else
+                    {
+                        if(i == DivisorMaximo)
+                        {
+                            ok = true;
+                        }
+                    }
+
+                }
+
+            }
+
+
+
+            return valor;
+        }
+
+
+    }
+}
---
+++ AlgolQ25/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgolQ25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DivisorMaximo = 20;
+            Console.WriteLine("O valor para "+ Convert.ToString(DivisorMaximo) 
+                              +" é " + Convert.ToString(Matematicas.RetornaMenorDivisivel(DivisorMaximo)));
+        }
+    }
+}
